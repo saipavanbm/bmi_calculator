a88e71cf937cd730f5e695071b116908f52204ee
saipavanbm <saipavanbm@gmail.com>
2020-10-22 18:39:31 +0530

    first commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,31 +5,31 @@ const labeltxtstyle = TextStyle(
   color: Color(0xFF8D8E98),
 );
 
-const bottomcontheight = 80.0;
-const activecardclr = Color(0xFF1D1E33);
-const inactivecardclr = Color(0xFF111328);
-const bottomcontclr = Color(0xFFEB1555);
+const bottomcontheight = 85.0;
+const activecardclr = Color(0xFF1D1E35);
+const inactivecardclr = Color(0xFF111356);
+const bottomcontclr = Color(0xFFEB1578);
 const kslidertxt = TextStyle(
-  fontSize: 50.0,
+  fontSize: 52.0,
   fontWeight: FontWeight.w900,
 );
 const klargebuttxt = TextStyle(
-  fontSize: 25.0,
-  fontWeight: FontWeight.bold,
+  fontSize: 28.0,
+  fontWeight: FontWeight.italic,
 );
 const kresultmaintxt = TextStyle(
   fontSize: 50.0,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.italic,
 );
 const kresulttxt = TextStyle(
   fontSize: 20.0,
   fontWeight: FontWeight.bold,
-  color: Color(0xFF24D876),
+  color: Color(0xFF24D866),
 );
 const kbmitxt = TextStyle(
-  fontSize: 100.0,
+  fontSize: 120.0,
   fontWeight: FontWeight.bold,
 );
 const kdesxt = TextStyle(
-  fontSize: 22.0,
+  fontSize: 24.0,
 );
